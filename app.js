9125d2af21b2d14ea1a13cc44c48d127679861aa
soufly9009 <jumelmaxime@gmail.com>
2026-08-21 20:33:22 +0200

    update readme.md

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ app.use("/", apiRouter);
 
 scriptRun(port);
 
+if (process.env.NODE_ENV === "development") {
+    console.log("Mode développement activé");
+    console.log("Port: ", port);
+    console.log("Database: ", db.name);
+} else if (process.env.NODE_ENV === "production") {
+    console.log("Mode production activé");
+    console.log("Port: ", port);
+    console.log("Database: ", db.name);
+}
 
-
+if(!process.env.JWT_SECRET){
+    console.error("Erreur : JWT_SECRET non défini");
+    process.exit(1);
+}
+if(!process.env.MONGODB_URI){
+    console.error("Erreur : MONGODB_URI non défini");
+    process.exit(1);
+}
 
 
